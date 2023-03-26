d39a887c7f7291b96a0fd131fd848b97db66b22d
IsaAdahanUnal <isaadahanunal@yeterly.com>
2023-03-26 05:35:06 +0300

    suggested music page changins

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -19,7 +19,7 @@ class _HomePageState extends State<HomePage> {
 
   List<Widget> pages = [
     const MapPage(),
-    const SuggestedMusicPage(),
+    SuggestedMusicPage(),
     const PeopleLikeYouPage(),
     const DatasPage()
   ];
